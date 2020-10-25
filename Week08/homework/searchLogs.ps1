ff61904bd10f8c11c39caafc793ea8c9a99b4203
WyattRuttle <54773930+WyattRuttle@users.noreply.github.com>
2020-10-25 17:48:25 -0400

    Add files via upload
    working version

--- Week08/homework/searchLogs.ps1
+++ Week08/homework/searchLogs.ps1
@@ -1,12 +1,14 @@
-# Storyline: Review the Security Event Log
+# Storyline: Review the Security Event Log and save to .csv file
 # Directory to save files:
 $myDir = "C:\Users\Admin\Documents\SYS-330\Files"
 
 #List all the available Windows Event Logs
 Get-EventLog -list 
 
-#Create a prompt to allow user to select the log to view
+#Prompt to allow user to select the log to view
 $readLog = Read-Host -Prompt "Please select a log to review from the list above"
+#Prompt to allow user to select search term in log
 $searchTerm = Read-Host -Prompt "Please write the term you would like to search"
-#Print the results for the log
-Get-EventLog -LogName $readLog -Newest 40 | Where-Object {$_.Message -match $searchTerm} | export-csv -NoTypeInformation -Path "$myDir\securityLogs.csv"
+
+#Print the results for the log and save to csv file
+Get-EventLog -LogName $readLog -Newest 40 | Where {$_.Message -ilike "*$searchTerm*" } | export-csv -NoTypeInformation -Path "$myDir\securityLogs.csv"
